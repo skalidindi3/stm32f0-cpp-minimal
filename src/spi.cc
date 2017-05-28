fcfba5cc2e2b76d10b895afd76a6a3efac2aaff9
Sai Kalidindi <skalidindi3@gmail.com>
2017-05-27 23:12:08 -0400

    fix whitespace for HAL SPI MspInit/DeInit

--- src/spi.cc
+++ src/spi.cc
@@ -6,26 +6,26 @@ extern "C" {
 
 // called by HAL_SPI_Init
 void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi) {
-  GPIO_InitTypeDef GPIO_InitStruct;
-  if(hspi->Instance == SPI1) {
-    /* Peripheral clock enable */
-    __HAL_RCC_SPI1_CLK_ENABLE();
+    GPIO_InitTypeDef GPIO_InitStruct;
+    if(hspi->Instance == SPI1) {
+        /* Peripheral clock enable */
+        __HAL_RCC_SPI1_CLK_ENABLE();
 
-    GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF0_SPI1;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-  }
+        GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
+        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF0_SPI1;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    }
 }
 
 // called by HAL_SPI_DeInit
 void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi) {
-  if(hspi->Instance == SPI1) {
-    __HAL_RCC_SPI1_CLK_DISABLE();
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);
-  }
+    if(hspi->Instance == SPI1) {
+        __HAL_RCC_SPI1_CLK_DISABLE();
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);
+    }
 }
 
 } // extern "C"
